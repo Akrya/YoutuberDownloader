ba82de360390c3eb733d449e21e8167b68644791
Aditya Krishnan <adityakrishnan99@gmail.com>
2023-04-20 19:42:41 +1000

    really slow and unoptimized, bad file management but it works. Completed basic project

--- main.py
+++ main.py
@@ -5,13 +5,35 @@ from pytube import YouTube
 
 def startDownload():
     try:
+        
         ytLink = link.get()
-        ytObject = YouTube(ytLink)
-        video = ytObject.streams.get_highest_resolution()
+        print("10")
+        ytObject = YouTube(ytLink, on_progress_callback=on_progress)
+        video = ytObject.streams.get_audio_only()
+        print("13")
+
+        title.configure(text= ytObject.title, text_color="white")
+        finishLabel.configure(text = "")
         video.download()
+        print("18")
+        finishLabel.configure(text = "Downloaded!")
     except:
-        print("Youtube link is invalid")
-    print("Download Complete!")
+        finishLabel.configure(text = "Download Error")
+
+
+def on_progress(stream, chunk, bytes_remaining):
+    total_size = stream.filesize
+    bytes_downloaded = total_size - bytes_remaining
+    percentage_of_completion = bytes_downloaded / total_size * 100
+    
+    #convert to string removes floating point and then conv to string to use it
+    per = str(int(percentage_of_completion))
+    pPercentage.configure(text = per + "%")
+    pPercentage.update()
+
+    #update progress bar
+    progressBar.set(float(percentage_of_completion)/100)
+    
 
 # System settings
 customtkinter.set_appearance_mode("System")
@@ -32,6 +54,20 @@ title.pack(padx = 10, pady=10)
 url_var = tkinter.StringVar()
 link = customtkinter.CTkEntry(app, width=350, height=40, textvariable=url_var)
 link.pack()
+
+#Finished Downloading
+finishLabel = customtkinter.CTkLabel(app, text = "")
+finishLabel.pack()
+
+#progress percentage
+pPercentage = customtkinter.CTkLabel(app, text = "0%")
+pPercentage.pack()
+
+progressBar = customtkinter.CTkProgressBar(app, width = 400)
+
+progressBar.set(0)
+progressBar.pack(padx = 10, pady = 10)
+
 
 #Download Button
 download = customtkinter.CTkButton(app, text="Download", command = startDownload)
