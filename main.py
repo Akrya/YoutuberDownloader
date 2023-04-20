d0922978b85367afd7be6994704258c8f303d4ea
Aditya Krishnan <adityakrishnan99@gmail.com>
2023-04-20 19:17:58 +1000

    changes to gitignore

--- main.py
+++ main.py
@@ -2,14 +2,23 @@ import tkinter
 import customtkinter
 from pytube import YouTube
 
-# System settings
 
+def startDownload():
+    try:
+        ytLink = link.get()
+        ytObject = YouTube(ytLink)
+        video = ytObject.streams.get_highest_resolution()
+        video.download()
+    except:
+        print("Youtube link is invalid")
+    print("Download Complete!")
+
+# System settings
 customtkinter.set_appearance_mode("System")
 customtkinter.set_default_color_theme("blue")
 
 
 #Our app frame
-
 app = customtkinter.CTk()
 app.geometry("720x480")
 app.title("Youtube Downloader")
@@ -25,8 +34,8 @@ link = customtkinter.CTkEntry(app, width=350, height=40, textvariable=url_var)
 link.pack()
 
 #Download Button
-#download = customtkinter.CTkButton(app, text="Download", command = startDownload)
-
+download = customtkinter.CTkButton(app, text="Download", command = startDownload)
+download.pack(padx = 10, pady=10)
 
 #Run app
 app.mainloop()
